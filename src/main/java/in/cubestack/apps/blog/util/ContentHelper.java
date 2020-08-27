9e5f158fab8a893d806bd390421f703b242058e3
Arun <bitsevn@gmail.com>
2020-08-27 23:23:25 +0530

    Added html table extension while translating from markdown to html

--- src/main/java/in/cubestack/apps/blog/util/ContentHelper.java
+++ src/main/java/in/cubestack/apps/blog/util/ContentHelper.java
@@ -1,16 +1,23 @@
 package in.cubestack.apps.blog.util;
 
+import org.commonmark.Extension;
+import org.commonmark.ext.gfm.tables.TableBlock;
+import org.commonmark.ext.gfm.tables.TablesExtension;
 import org.commonmark.node.Node;
 import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.AttributeProvider;
 import org.commonmark.renderer.html.HtmlRenderer;
 
 import javax.enterprise.context.ApplicationScoped;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
 @ApplicationScoped
 public class ContentHelper {
 
     private static final int MAX_LENGTH = 120;
-
+    private List<Extension> extensions;
     private Parser parser;
     private final Slugify slugify;
 
@@ -30,15 +37,29 @@ public class ContentHelper {
 
     public String markdownToHtml(String markdown) {
         Node document = getParser().parse(markdown);
-        HtmlRenderer renderer = HtmlRenderer.builder().build();
+        HtmlRenderer renderer = HtmlRenderer
+                .builder()
+                .attributeProviderFactory(ctx -> new TableAttributeProvider())
+                .extensions(extensions)
+                .build();
         return renderer.render(document);
+    }
+
+    private class TableAttributeProvider implements AttributeProvider {
+        @Override
+        public void setAttributes(Node node, String s, Map<String, String> attributes) {
+            if (node instanceof TableBlock) {
+                attributes.put("class", "table table-sm sj-table sj-markdown-table");
+            }
+        }
     }
 
     private Parser getParser() {
         if (parser == null) {
             synchronized (ContentHelper.class) {
                 if (parser == null) {
-                    parser = Parser.builder().build();
+                    extensions = Arrays.asList(TablesExtension.create());
+                    parser = Parser.builder().extensions(extensions).build();
                 }
             }
         }
